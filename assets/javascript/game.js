8761ca96cd382c1df0cc563833f7baa03379dd4b
Frank McDonald <Frank McDonald>
2017-05-04 13:17:44 -0700

    js and html updates

---
+++ assets/javascript/game.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+            crystals = ["assets/images/red.png", "assets/images/orange.png", "assets/images/yellow.png", "assets/images/blue.png", ]
+                // create wins
+            var wins = 0;
+            // create losses
+            var losses = 0;
+            // total guess score
+            var guessCounter = 0;
+            // create crystal random values
+
+            $("#wins").text(wins);
+            $("#losses").text(losses);
+
+            //New game function
+            // 
+            var randomNumber = Math.floor(Math.random()) *
+            //New crystal function, randomize value to img files
+            
+            //Counter to add guesses
+
+            // If guess is correct you win
+
+            // Else guess is incorrect you lose (newGame)
+
+      };
